bf51c542ab2e669e7cf633b526d7d457b5687a7e
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-06-25 15:32:24 +0530

    fix: improved readability

--- packages/demo-app/src/App.js
+++ packages/demo-app/src/App.js
@@ -1,11 +1,12 @@
-import Login from "./Components/Login";
+import Login from './Components/Login'
+import Dashboard from './Components/Dashboard'
+import Signup from './Components/Signup'
 function App() {
   return (
     <div className="Container">
-    <Login>
-    </Login>
+      <Login></Login>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
