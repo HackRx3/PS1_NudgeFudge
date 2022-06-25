593282e428d1493061b40056ee3ab4747513a888
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-06-25 22:18:11 +0530

    chore: Added Dashboard nroute

--- packages/demo-app/src/App.js
+++ packages/demo-app/src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<div>Home</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
