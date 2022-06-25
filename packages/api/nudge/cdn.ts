9e02a98b850cccdb558783c36b456e417998797f
jagnani73 <yashjagnani73@gmail.com>
2022-06-25 13:04:44 +0530

    feat(be): add dot nudges

--- packages/api/nudge/cdn.ts
+++ packages/api/nudge/cdn.ts
@@ -6,50 +6,92 @@ console.log(app_id);
 //? INFO: dummy data
 const nudges = [
   {
-    type: "overlay",
-    background_color: "#232323",
-  },
-  {
-    type: "dot",
-    elementId: "rewards_logo",
-    background_color: "#232323",
+    event_label: "success",
+    nudge: {
+      label: "green_dot",
+      type: "dot",
+      config: {
+        elementId: "rewards_logo",
+        backgroundColor: "#232323",
+        size: "8px",
+        position: "top-middle",
+      },
+    },
   },
 ];
 
 //? INFO: loop through the nudges to set them up
-nudges.forEach((nudge) => {
-  switch (nudge.type) {
+nudges.forEach(({ nudge: { type, config } }) => {
+  switch (type) {
     case "dot": {
-      const parent = document.getElementById(nudge.elementId!);
+      const { elementId, ...style } = config;
+
+      const parent = document.getElementById(elementId!);
       parent!.style.position = "relative";
 
       const dotNudge = document.createElement("div");
+
+      dotNudge.style.backgroundColor = style.backgroundColor;
+
       dotNudge.style.zIndex = "99999";
       dotNudge.style.position = "absolute";
-      dotNudge.style.width = "10px";
-      dotNudge.style.height = "10px";
       dotNudge.style.borderRadius = "100%";
-      dotNudge.style.top = "0";
-      dotNudge.style.right = "0";
-      dotNudge!.style.backgroundColor = nudge.background_color!;
+
+      dotNudge.style.width = style.size!;
+      dotNudge.style.height = style.size!;
+
+      if (style.position === "top-left") {
+        dotNudge.style.left = "0";
+        dotNudge.style.top = "0";
+      } else if (style.position === "top-middle") {
+        dotNudge.style.top = "0";
+        dotNudge.style.left = "50%";
+        dotNudge.style.transform = "translateX(-50%)";
+      } else if (style.position === "top-right") {
+        dotNudge.style.right = "0";
+        dotNudge.style.top = "0";
+      } else if (style.position === "middle-left") {
+        dotNudge.style.left = "0";
+        dotNudge.style.top = "50%";
+        dotNudge.style.transform = "translateY(-50%)";
+      } else if (style.position === "middle-middle") {
+        dotNudge.style.top = "50%";
+        dotNudge.style.transform = "translateY(-50%)";
+        dotNudge.style.left = "50%";
+        dotNudge.style.transform = "translateX(-50%)";
+      } else if (style.position === "middle-right") {
+        dotNudge.style.right = "0";
+        dotNudge.style.top = "50%";
+        dotNudge.style.transform = "translateY(-50%)";
+      } else if (style.position === "bottom-left") {
+        dotNudge.style.left = "0";
+        dotNudge.style.bottom = "0";
+      } else if (style.position === "bottom-middle") {
+        dotNudge.style.bottom = "0";
+        dotNudge.style.left = "50%";
+        dotNudge.style.transform = "translateX(-50%)";
+      } else if (style.position === "bottom-right") {
+        dotNudge.style.right = "0";
+        dotNudge.style.bottom = "0";
+      }
 
       parent?.appendChild(dotNudge);
 
       break;
     }
 
-    case "overlay": {
-      const backdrop = document.createElement("div");
-      backdrop.style.backgroundColor = "#232323";
-      backdrop.style.zIndex = "99999";
-      backdrop.style.position = "absolute";
-      backdrop.style.top = "0";
-      backdrop.style.left = "0";
-      backdrop.style.width = "100%";
-      backdrop.style.height = "100%";
-      document.body.appendChild(backdrop);
-      break;
-    }
+    // case "overlay": {
+    //   const backdrop = document.createElement("div");
+    //   backdrop.style.backgroundColor = "#232323";
+    //   backdrop.style.zIndex = "99999";
+    //   backdrop.style.position = "absolute";
+    //   backdrop.style.top = "0";
+    //   backdrop.style.left = "0";
+    //   backdrop.style.width = "100%";
+    //   backdrop.style.height = "100%";
+    //   document.body.appendChild(backdrop);
+    //   break;
+    // }
   }
 });
 
--- packages/api/nudge/nudge.service.ts
+++ packages/api/nudge/nudge.service.ts
@@ -4,7 +4,7 @@ import { PostNudgeType } from "./nudge.schema";
 
 export const addNudge = async (data: PostNudgeType): Promise<boolean> => {
   const db = await DatabaseService.getInstance().getDb("nudges");
-  const exists = await db.findOne({ nudge_label: data.nudge_label });
+  const exists = await db.findOne({ nudge_label: data.nudge.label });
   if (exists) throw errors.CONFLICT;
   await db.insertOne(data);
   return true;
