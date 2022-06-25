4f79f948a3a4e42c04a81c5b49accccb367f6786
jagnani73 <yashjagnani73@gmail.com>
2022-06-25 16:55:56 +0530

    fix(be): fetch nudge

--- packages/api/nudge/nudge.service.ts
+++ packages/api/nudge/nudge.service.ts
@@ -12,5 +12,5 @@ export const addNudge = async (data: PostNudgeType): Promise<boolean> => {
 
 export const getNudgesBasedOnAppId = async (app_id: string): Promise<any> => {
   const db = await DatabaseService.getInstance().getDb("nudges");
-  return await db.find({ app_id }).toArray();
+  return await db.find({ app_id, event_label: { $exists: false } }).toArray();
 };
