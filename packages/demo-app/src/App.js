bbb4d5a1078acf293ed87072583cc9a8e7aeff77
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-06-25 15:41:35 +0530

    init(screens): Initialized new screens

--- packages/demo-app/src/App.js
+++ packages/demo-app/src/App.js
@@ -1,11 +1,10 @@
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
 function App() {
-  return (
-    <div className="Container">
-    <Login>
-    </Login>
-    </div>
-  );
+  return <div className="Container">
+    <Dashboard />
+  </div>;
 }
 
 export default App;
