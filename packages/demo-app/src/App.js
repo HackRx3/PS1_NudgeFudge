f4c8a0f17afeb77b6251276e9bf10025a07e35d7
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-06-25 15:52:11 +0530

    Fix: UI fixed

--- packages/demo-app/src/App.js
+++ packages/demo-app/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react'
+
 import Login from "./Components/Login";
-import Signup from "./Components/Signup";
-import Dashboard from "./Components/Dashboard";
+
 function App() {
-  return <div className="Container">
-    <Dashboard />
-  </div>;
+  return (
+    <div className="Container">
+    <Login>
+    </Login>
+    </div>
+  );
 }
 
 export default App;
