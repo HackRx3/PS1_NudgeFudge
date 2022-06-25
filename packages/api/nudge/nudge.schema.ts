a78c005f4d579c401a94435cdfe58514b2c4bc51
jagnani73 <yashjagnani73@gmail.com>
2022-06-25 13:50:00 +0530

    feat(be): add overlay nudges

--- packages/api/nudge/nudge.schema.ts
+++ packages/api/nudge/nudge.schema.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const TypesOfNudges = ["dot", "overlay"];
 
-const NudgePosition = [
+const NUDGE_POSITION = [
   "top-left",
   "top-middle",
   "top-right",
@@ -14,11 +14,13 @@ const NudgePosition = [
   "bottom-right",
 ];
 
+const TEXT_ALIGNMENT = ["left", "center", "right", "justify"];
+
 export const dotNudgeSchema = yup
   .object({
     elementId: yup.string().trim().required(),
     backgroundColor: yup.string().trim().default("#0067b5"),
-    position: yup.string().trim().oneOf(NudgePosition).default("top-right"),
+    position: yup.string().trim().oneOf(NUDGE_POSITION).default("top-right"),
     size: yup.string().trim().default("12px"),
   })
   .noUnknown(true)
@@ -26,8 +28,17 @@ export const dotNudgeSchema = yup
 
 export const overlayNudgeSchema = yup
   .object({
-    children: yup.string().trim(),
-    border_color: yup.string().trim(),
+    text: yup.string().trim().required(),
+    textAlign: yup.string().trim().oneOf(TEXT_ALIGNMENT).default("left"),
+    fontSize: yup.string().trim().default("10.pt"),
+    color: yup.string().trim().default("#1A1A1A"),
+    backdropColor: yup.string().trim().default("#23232375"),
+    backgroundColor: yup.string().trim().default("##FAFAFA"),
+    borderColor: yup.string().trim().default("#0067b575"),
+    borderWidth: yup.string().trim().default("0.5rem"),
+    borderRadius: yup.string().trim().default("4rem"),
+    width: yup.string().trim().default("85%"),
+    height: yup.string().trim().default("fit-content"),
   })
   .noUnknown(true)
   .required();
