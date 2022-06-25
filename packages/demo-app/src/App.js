5083baaef5bba96fba777fa4e53c2724d36dff58
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-06-25 19:16:20 +0530

    feat: CDN scripts

--- packages/demo-app/src/App.js
+++ packages/demo-app/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
-    <div className="Container">
-    <Login>
-    </Login>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
--- packages/demo-app/src/Components/Login.jsx
+++ packages/demo-app/src/Components/Login.jsx
@@ -1,20 +1,27 @@
+import "../styles/Login.css";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="loginForm">
-      <h2 className="header">Login Portal</h2>
-      <br></br>
-      {/* add email id and password */}
-      <h4>
-        Email ID: <input />
-      </h4>
-      <h4>
-        Password: <input />
-      </h4>
-      <button className="btn">Log In</button>
-      <br></br>
-      <button className="btn">New? Sign Up here</button> <br></br>
+    <div className="card">
+      <div className="loginForm">
+        <h2 className="title">Login Portal</h2>
+        <br></br>
+        {/* add email id and password */}
+        <h4>
+          Email ID:{" "}
+          <input style={{ padding: 10, margin: 5, borderRadius: 10 }} />
+        </h4>
+        <h4>
+          Password:{" "}
+          <input style={{ padding: 10, margin: 5, borderRadius: 10 }} />
+        </h4>
+        <button className="btn" id="rewards_logo">
+          Log Innn
+        </button>
+        <br></br>
+        <button className="btn">New? Sign Up here</button> <br></br>
+      </div>
     </div>
   );
 };
