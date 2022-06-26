3cf8959e6f4bdafcfa8652b7aff6a187bd1e196c
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-06-26 07:29:53 +0530

    chore: fixed zoom and refactored

--- packages/flutter/NudgeFudge/lib/screens/HomePage.dart
+++ packages/flutter/NudgeFudge/lib/screens/HomePage.dart
@@ -9,35 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  String _url= 'https://resplendent-sopapillas-d20071.netlify.app/';
+  String _url = 'https://resplendent-sopapillas-d20071.netlify.app/';
 
   WebViewController? _webViewController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        setState(() {
-          _url = 'https://resplendent-sopapillas-d20071.netlify.app/';
-          _webViewController!.loadUrl(_url);
-          print("Loaded URL: $_url");
-        });
-      }, child: Icon(Icons.refresh,size: 30,),),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white10,
+          onPressed: () {
+            setState(() {
+              _url = 'https://resplendent-sopapillas-d20071.netlify.app/';
+              _webViewController!.loadUrl(_url);
+              print("Loaded URL: $_url");
+            });
+          },
+          child: const Icon(
+            Icons.refresh,
+            size: 30,
+          ),
+        ),
         body: WebView(
-      initialUrl: _url,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webviewController) {
-        _webViewController = webviewController;
-      },
-      onProgress: (int progress) {
-        print('WebView is loading (progress : $progress%)');
-      },
-      onPageStarted: (String url) {
-        print('Page started loading: $url');
-      },
-      onPageFinished: (String url) {
-        print('Page finished loading: $url');
-      },
-    ));
+          zoomEnabled: true,
+          initialUrl: _url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webviewController) {
+            _webViewController = webviewController;
+          },
+          onProgress: (int progress) {
+            print('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+        ));
   }
 }
